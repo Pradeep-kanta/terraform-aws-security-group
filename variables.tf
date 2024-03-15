be9cf48be8a79ea286e2eda69cece192ef78f0ca
Pradeep-kanta <kantapradeep07@gmail.com>
2024-03-15 11:48:59 +0530

    aws-sg

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "project_name" {
+  
+}
+
+variable "environment" {
+  
+}
+
+variable "sg_name" {
+ # default = mongodb
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "sg_description" {
+  type = string
+}
+
+variable "common_tags" {
+   type = map
+   default = {}
+}
+
+variable "sg_tags" {
+   type = map
+   default = {}
+}
+
+variable "sg_ingress_rules" {
+  type = list
+  default = []
+}
